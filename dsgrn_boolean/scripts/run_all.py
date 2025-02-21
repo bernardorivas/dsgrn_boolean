923837c840359adb1d129a7c599a3679c1cf293c
Bernardo Rivas <46282518+bernardorivas@users.noreply.github.com>
2025-02-20 20:22:05 -0500

    Renamed figures for clarity

--- dsgrn_boolean/scripts/run_all.py
+++ dsgrn_boolean/scripts/run_all.py
@@ -1,7 +1,8 @@
 import subprocess
 
-# par_list = [0, 49, 98, 147]
-par_list = [49, 98, 147] # running the missing ones overnight
+par_list = [0, 49, 98, 147]
+# par_list = [49, 98, 147] # running the missing ones overnight
+# par_list = [ 0, 147] # running the easy ones while at work
 
 for par_index in par_list:
     print(f"Running stability analysis for parameter index: {par_index}")
