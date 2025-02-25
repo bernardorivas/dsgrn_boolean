c752d67625e3c91378994753e7e9678383cab908
Bernardo Rivas <46282518+bernardorivas@users.noreply.github.com>
2025-02-25 11:14:35 -0500

    Changed default parameter to 98

--- dsgrn_boolean/scripts/run_all.py
+++ dsgrn_boolean/scripts/run_all.py
@@ -1,8 +1,8 @@
 import subprocess
 
 par_list = [0, 49, 98, 147]
-# par_list = [49, 98, 147] # running the missing ones overnight
-# par_list = [ 0, 147] # running the easy ones while at work
+# par_list = [ 0, 98 ] # gotta run at 10000 samples
+# par_list = [ 49, 147] # already ran at 10000 samples
 
 for par_index in par_list:
     print(f"Running stability analysis for parameter index: {par_index}")
